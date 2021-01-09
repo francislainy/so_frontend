86593819440180c50c29a8859e297dc81608168d
Francislainy Campos <francislainy.campos@hmhco.com>
2021-01-09 06:07:40 +0000

    Display items from api

---
+++ src/api/api.js
@@ -0,0 +1,15 @@
+"use strict"
+
+const axios = require("axios")
+
+exports.getQuestionList = endpoint => {
+    const url = endpoint.url
+    const port = endpoint.port
+
+    return axios.request({
+        method: "GET",
+        baseURL: `${url}:${port}`,
+        url: "api/so/question",
+        headers: {Accept: "application/json"},
+    })
+}
--- src/components/PostList/PostList.js
+++ src/components/PostList/PostList.js
@@ -1,16 +1,52 @@
-import PostItem from '../PostItem/PostItem'
+import React, {useEffect, useState} from 'react';
 
-const data = [
-    { id: 1, name: "Leanne Graham" },
-    { id: 2, name: "Ervin Howell" },
-    { id: 3, name: "Clementine Bauch" },
-    { id: 4, name: "Patricia Lebsack" }
-];
+import PostItem from '../PostItem/PostItem'
+import {getQuestionList} from '../../api/api'
+
+// const data = [
+//     {id: 1, name: "Leanne Graham"},
+//     {id: 2, name: "Ervin Howell"},
+//     {id: 3, name: "Clementine Bauch"},
+//     {id: 4, name: "Patricia Lebsack"}
+// ];
 
 function PostList() {
+
+    const [data, setData] = useState(
+        {
+            "questions": [
+                {
+                    "id": "",
+                    "title": "",
+                    "creationDate": 0
+                }
+            ]
+
+        });
+
+    useEffect(() => {
+
+        const axiosParams = {
+            url: 'http://localhost',
+            port: 8081,
+        }
+
+        getQuestionList(axiosParams)
+
+            .then(({data}) => {
+                    setData({...data})
+                    console.log('success')
+                }
+            ).catch(reason => {
+            console.log(reason + ' failure on retrieving question list')
+        })
+
+    }, [])
+
+
     return (
         <ul>
-            {data.map((item) => <PostItem key={item.id} value={item.name}/>)}
+            {data.questions.map((item) => <PostItem key={item.id} value={item.title}/>)}
         </ul>
     );
 }
